0bf8a26c3bc4b698db6b9818cdbe93d018f0f73c
Aysha Hussain <ayshafajar3@gmail.com>
2023-01-27 19:43:51 +0000

    deployment-tests-5

--- server/authorization-middleware.js
+++ server/authorization-middleware.js
@@ -1,8 +1,7 @@
-const jwt = require('jsonwebtoken'); // eslint-disable-line
-const ClientError = require('./client-error'); // eslint-disable-line
+const jwt = require('jsonwebtoken');
+const ClientError = require('./client-error');
 
 function authorizationMiddleware(req, res, next) {
-  /* your code here */
   const token = req.get('X-Access-Token');
   if (!token) {
     throw new ClientError(401, 'authentication required');
